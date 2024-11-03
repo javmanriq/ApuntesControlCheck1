61b0c6d40bbab684ef7f5e8e62a475a813494780
Javier Manrique Riquelme <Javier.Manrique688@gmail.com>
2024-11-03 23:44:25 +0100

    Test2 (ProductType)

--- src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
@@ -3,6 +3,7 @@ package org.springframework.samples.petclinic.product;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
@@ -11,4 +12,7 @@ public interface ProductRepository extends CrudRepository<Product, Integer> {
     List<Product> findAll();
 
     Product save(Product p);
+
+    @Query("SELECT pt FROM ProductType pt")
+    List<ProductType> findAllProductTypes();
 }
--- src/main/java/org/springframework/samples/petclinic/product/ProductType.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductType.java
@@ -1,10 +1,22 @@
 package org.springframework.samples.petclinic.product;
 
+import org.springframework.samples.petclinic.model.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class ProductType {
+@Entity
+public class ProductType extends BaseEntity {
+    @NotNull
+    @NotBlank
+    @Size(min = 1, max = 50)
+    @Column(unique = true)
     String name;
 }
