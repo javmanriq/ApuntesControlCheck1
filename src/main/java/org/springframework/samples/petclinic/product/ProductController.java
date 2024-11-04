5fb0f691126e0515349443b7ee04b4de4d0b9c82
Javier Manrique Riquelme <Javier.Manrique688@gmail.com>
2024-11-04 13:07:43 +0100

    Test7 (POST)

--- src/main/java/org/springframework/samples/petclinic/product/ProductController.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductController.java
@@ -1,5 +1,29 @@
 package org.springframework.samples.petclinic.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+
+@RestController
 public class ProductController {
+
+    @Autowired
+    ProductService ps;
+
+    @PostMapping("/api/v1/products")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product createProduct(@RequestBody @Valid Product p) throws UnfeasibleProductUpdate { 
+        ps.save(p);
+        return p;
+    }
+    
+
+
     
 }
